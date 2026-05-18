61c7e6a24cc506a087a8428e2d20d559a82c279e
Barmaley <Dakos4k@gmail.com>
2026-05-18 20:28:28 +0300

    Add marker thread data structures

--- OS_Lab3_Java/MarkerThreadsLab.java
+++ OS_Lab3_Java/MarkerThreadsLab.java
@@ -1,8 +1,42 @@
 package OS_Lab3_Java;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MarkerThreadsLab {
+
+    private static class MarkerData {
+    int id;
+    int markedCount;
+    int blockedIndex;
+    boolean terminated;
+
+        MarkerData(int id) {
+            this.id = id;
+            this.markedCount = 0;
+            this.blockedIndex = -1;
+            this.terminated = false;
+        }
+    }
+
+    private static class MarkerThread extends Thread {
+        private final MarkerData data;
+        private final int[] array;
+
+        MarkerThread(MarkerData data, int[] array) {
+            this.data = data;
+            this.array = array;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(
+                "Marker thread #" + data.id + " started."
+            );
+        }
+    }
+
     private static int readPositiveInt(
         Scanner scanner,
         String message
@@ -41,28 +75,25 @@ public class MarkerThreadsLab {
     }
 
     public static void main(String[] args) {
-        Scanner scanner;
+        Scanner scanner = new Scanner(System.in);
 
-        int arraySize;
-        int markerCount;
+        int arraySize = readPositiveInt(scanner, "Enter array size: ");
+        int markerCount = readPositiveInt(scanner, "Enter marker thread count: ");
 
-        int[] array;
+        int[] array = new int[arraySize];
 
-        scanner = new Scanner(System.in);
+        List<MarkerData> markerDataList = new ArrayList<MarkerData>();
+        List<MarkerThread> markerThreads = new ArrayList<MarkerThread>();
 
-        arraySize = readPositiveInt(
-            scanner,
-            "Enter array size: "
-        );
+        for (int i = 0; i < markerCount; i++) {
+            MarkerData data = new MarkerData(i + 1);
+            MarkerThread thread = new MarkerThread(data, array);
 
-        markerCount = readPositiveInt(
-            scanner,
-            "Enter marker thread count: "
-        );
+            markerDataList.add(data);
+            markerThreads.add(thread);
 
-        array = new int[arraySize];
-
-        printArray(array);
+            thread.start();
+        }
 
         scanner.close();
     }
